4143acb020b8af9d2c5c1e297bb071e1e324abb5
Colten <129473270+Davittio-L@users.noreply.github.com>
2024-02-05 22:12:14 -0500

    Completed logic for 'click' event listner

--- script.js
+++ script.js
@@ -3,7 +3,20 @@ var input = document.getElementById("userInput");
 var ul = document.querySelector("ul");
 
 button.addEventListener("click", function() {
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode("Cheeseburgers"));
+    if (input.value.length > 0) {
+        var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
     ul.appendChild(li);
+    input.value = "";
+    }
+})
+
+button.addEventListener("keydown", function() {
+    event.which
+    if (input.value.length > 0) {
+        var li = document.createElement("li");
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = "";
+    }
 })
