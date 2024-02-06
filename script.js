c1bb2f306821be877548ce4cc396299e3d8e614a
Colten <129473270+Davittio-L@users.noreply.github.com>
2024-02-05 22:16:31 -0500

    completed logic & functionality for events

--- script.js
+++ script.js
@@ -11,12 +11,11 @@ button.addEventListener("click", function() {
     }
 })
 
-button.addEventListener("keydown", function() {
-    event.which
-    if (input.value.length > 0) {
+input.addEventListener("keydown", function (event) {
+    if (input.value.length > 0 && event.key === "Enter") {
         var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value = "";
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        input.value = "";
     }
 })
